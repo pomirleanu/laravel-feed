9f5461fa1fe19d441c47c9697827486b458c88af
Pomirleanu Florentin Cristinel <pomirleanu.florentin@gmail.com>
2016-06-11 12:08:54 +0300

    Fix For Images In Content Atom

--- src/views/atom.blade.php
+++ src/views/atom.blade.php
@@ -1,19 +1,19 @@
 {!! '<'.'?'.'xml version="1.0" encoding="UTF-8" ?>' !!}
-<feed xmlns="http://www.w3.org/2005/Atom"<?php foreach($namespaces as $n) echo " ".$n; ?>>
+<feed xmlns="http://www.w3.org/2005/Atom"<?php foreach ($namespaces as $n) echo " " . $n; ?>>
     <title type="text">{!! $channel['title'] !!}</title>
     <subtitle type="html"><![CDATA[{!! $channel['description'] !!}]]></subtitle>
     <link href="{{ Request::url() }}"></link>
     <id>{{ $channel['link'] }}</id>
-    <link rel="alternate" type="text/html" href="{{ Request::url() }}" ></link>
-    <link rel="self" type="application/atom+xml" href="{{ $channel['link'] }}" ></link>
-@if (!empty($channel['logo']))
-    <logo>{{ $channel['logo'] }}</logo>
-@endif
-@if (!empty($channel['icon']))
-    <icon>{{ $channel['icon'] }}</icon>
-@endif
-        <updated>{{ $channel['pubdate'] }}</updated>
-@foreach($items as $item)
+    <link rel="alternate" type="text/html" href="{{ Request::url() }}"></link>
+    <link rel="self" type="application/atom+xml" href="{{ $channel['link'] }}"></link>
+    @if (!empty($channel['logo']))
+        <logo>{{ $channel['logo'] }}</logo>
+    @endif
+    @if (!empty($channel['icon']))
+        <icon>{{ $channel['icon'] }}</icon>
+    @endif
+    <updated>{{ $channel['pubdate'] }}</updated>
+    @foreach($items as $item)
         <entry>
             <author>
                 <name>{{ $item['author'] }}</name>
@@ -22,8 +22,21 @@
             <link rel="alternate" type="text/html" href="{{ $item['link'] }}"></link>
             <id>{{ $item['link'] }}</id>
             <summary type="html"><![CDATA[{!! $item['description'] !!}]]></summary>
-            <content type="html"><![CDATA[{!! $item['content'] !!}]]></content>
+            <content type="html">
+                <![CDATA[
+                @if(is_array($item['content']))
+                    @foreach($item['content'] as $page)
+                        <a href="{{$channel['link']}}">
+                            <img alt="{{$item['description']}}"
+                                 src="{{asset(str_lreplace("img","thumbs/img",$page->image))}}"/>
+                        </a>
+                    @endforeach
+                @else
+                    <![CDATA[{!! $item['content'] !!}]]
+                @endif
+                ]]>
+            </content>
             <updated>{{ $item['pubdate'] }}</updated>
         </entry>
-@endforeach
+    @endforeach
 </feed>
